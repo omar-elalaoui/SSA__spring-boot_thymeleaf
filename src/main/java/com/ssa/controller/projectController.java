14bad639c251ce26a2f24925598f12d4b64999c1
oma el alaoui <omar.elalaoui.97@gmail.com>
2019-10-05 19:03:21 +0100

    first commit

---
+++ src/main/java/com/ssa/controller/projectController.java
@@ -0,0 +1,32 @@
+package com.ssa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class projectController {
+    
+    @GetMapping("/fiche")
+    public String project_fiche(Model model) {
+        return "project_fiche";
+    }
+    
+    @GetMapping("/description")
+    public String project_desc(Model model) {
+        return "project_desc";
+    }
+    
+    @GetMapping("/suivi_chantier")
+    public String project_suivi(Model model) {
+        return "project_suivi_chantier";
+    }
+    
+    @GetMapping("/pices")
+    public String project_pices(Model model) {
+        return "project_pices";
+    }
+    
+}
