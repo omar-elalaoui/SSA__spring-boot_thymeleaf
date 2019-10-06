5a3787cc0236496c5a8d4f2b333413807fb3b551
Ismail <roadtoneos@gmail.com>
2019-10-06 18:40:17 +0000

    implementation multipartservice --finished

--- src/main/java/com/ssa/service/impl/ProjetServiceImpl.java
+++ src/main/java/com/ssa/service/impl/ProjetServiceImpl.java
@@ -49,16 +49,39 @@ public class ProjetServiceImpl implements ProjetService {
     
     @Override
     public Phase2 convertToPhase2(Phase2MultipartFiles files) {
+
+        Phase2 phase2= new Phase2();
+        Document releve_existant= documentService.convertFileToDocument(files.getreleve_existant());
+        Document photo_site= documentService.convertFileToDocument(files.getphoto_site());
+        Document esquisse= documentService.convertFileToDocument(files.getesquisse());
+        Document plan_dwg= documentService.convertFileToDocument(files.getplan_dwg());
+
         return null;
     }
     
     @Override
     public Phase3 convertToPhase3(Phase3MultipartFiles files) {
+
+        Phase3 phase3 = new Phase3();
+        Document autorization= documentService.convertFileToDocument(files.getautorization());
+        Document pv_commition= documentService.convertFileToDocument(files.getpv_commition());
+        Document plan_approuve= documentService.convertFileToDocument(files.getplan_approuve());
+        Document attestation_impl= documentService.convertFileToDocument(files.getattestation_impl());
+        Document plan_beton_arme= documentService.convertFileToDocument(files.getplan_beton_arme());
+        Document pv_suivi= documentService.convertFileToDocument(files.getpv_suivi());
+        Document photo_exec= documentService.convertFileToDocument(files.getphoto_exec());
+
         return null;
     }
     
     @Override
     public Phase4 convertToPhase4(Phase4MultipartFiles files) {
+
+        Phase4 phase4= new Phase4();
+        Document photo_acheve= documentService.convertFileToDocument(files.getphoto_acheve());
+        Document fermeture_chantier= documentService.convertFileToDocument(files.getfermeture_chantier());
+        Document attestation_fin_travaux= documentService.convertFileToDocument(files.getattestation_fin_travaux());
+
         return null;
     }
 }
